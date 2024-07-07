aa6bdefefb3ac03086a245c31a956833bf7497a8
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-07-07 10:10:12 +0300

    Set up project assessts

---
+++ app/index.jsx
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {Link} from 'expo-router';
+
+export default function App() {
+  return (
+    <View className="bg-primary flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Aora!</Text>
+      <StatusBar style='auto' />
+      <Link href="/profile" className="text-red-500" >Go to Profile</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
