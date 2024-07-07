eb79b7541504b350e9ae85ffaf0a8896bc38363a
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-07-07 11:23:41 +0300

    Design onboarding screen

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -13,7 +13,8 @@ const TabIcon = ({ icon, color, name, focused }) => {
         className='w-6 h-6'
       />
       <Text
-        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`} style={{color: color}}
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }}
       >
         {name}
       </Text>
@@ -24,12 +25,19 @@ const TabIcon = ({ icon, color, name, focused }) => {
 const TabsLayout = () => {
   return (
     <>
-      <Tabs screenOptions={{ tabBarShowLabel: false, tabBarActiveTintColor: "#FFA001", tabBarInactiveTintColor: '#CDCDE0', tabBarStyle: {
-        backgroundColor: '#161622',
-        borderTopWidth: 1,
-        borderTopColor: '#232533',
-        height: 84,
-      } }}>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarStyle: {
+            backgroundColor: "#161622",
+            borderTopWidth: 1,
+            borderTopColor: "#232533",
+            height: 84,
+          },
+        }}
+      >
         <Tabs.Screen
           name='home'
           options={{
