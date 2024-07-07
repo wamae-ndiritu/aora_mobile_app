0f9d07ea8f4119c9af720bc6f00b537d5be38129
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-07-07 10:34:59 +0300

    Handle navigation tabs

--- app/index.jsx
+++ app/index.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <View className="bg-primary flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pblack">Aora!</Text>
       <StatusBar style='auto' />
-      <Link href="/profile" className="text-red-500" >Go to Profile</Link>
+      <Link href="/home" className="text-red-500" >Go to Profile</Link>
     </View>
   );
 }
