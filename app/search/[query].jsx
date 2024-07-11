68cec3be5519b2cece08fea9b1a3a5feac8cc4c8
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-07-11 23:43:03 +0300

    Add search video feature

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,12 +1,59 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  RefreshControl,
+  Alert,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import SearchInput from "../../components/SearchInput";
+import Trending from "../../components/Trending";
+import EmptyState from "../../components/EmptyState";
+import { getAllPosts, getLatestPosts, searchPosts } from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
+import VideoCard from "../../components/VideoCard";
+import { StatusBar } from "expo-status-bar";
+import { useLocalSearchParams } from "expo-router";
 
 const Search = () => {
-  return (
-    <View>
-      <Text>Search</Text>
-    </View>
-  )
-}
+  const { query } = useLocalSearchParams();
+  const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
 
-export default Search
+
+  useEffect(() => {
+    refetch();
+  }, [query]);
+
+  return (
+    <SafeAreaView className='bg-primary h-full'>
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className='my-6 px-4 space-y-6'>
+            <Text className='font-pmedium text-sm text-gray-100'>
+              Search Results
+            </Text>
+            <Text className='text-2xl font-psemibold text-white'>{query}</Text>
+            <View className='mt-6 mb-8'>
+              <SearchInput initialQuery={query} />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title='No Videos found'
+            subtitle='No videos found for this search'
+          />
+        )}
+      />
+      <StatusBar backgroundColor='#161622' style='light' />
+    </SafeAreaView>
+  );
+};
+
+export default Search;
